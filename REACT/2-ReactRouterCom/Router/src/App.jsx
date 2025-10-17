2122adfdb0da0f2360e06052db40b46345b245b2
jferalo2907 <jferalo2907@g.educaand.es>
2025-10-17 16:23:28 +0200

    muchos

--- REACT/2-ReactRouterCom/Router/src/App.jsx
+++ REACT/2-ReactRouterCom/Router/src/App.jsx
@@ -1,11 +1,12 @@
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    
+   
       <div>
-     
-      </div>
+       
+    </div>
   )
 }
 
--- REACT/2-ReactRouterCom/Router/src/main.jsx
+++ REACT/2-ReactRouterCom/Router/src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+import App from './App.jsx';
+import "./styles/stylesGlobales.css";
+import "./styles/stylesReusables.css";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
